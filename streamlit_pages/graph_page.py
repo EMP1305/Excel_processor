55e50ab118e45b0e04950c7b8a0d9ba5014704ab
Ernesto Marrero Pérez <emp.2005.qba@gmail.com>
2025-05-27 22:17:10 -0400

    fix in graph_page

--- streamlit_pages/graph_page.py
+++ streamlit_pages/graph_page.py
@@ -3,8 +3,8 @@ from df_show import data_frames
 import streamlit
 import pandas
 
-fig,ax = matplotlib.pyplot.subplots()
 for key in data_frames:
+    fig,ax = matplotlib.pyplot.subplots()
     data_frame: pandas.DataFrame = data_frames [key]
     x = data_frame.index.values
     for column in data_frame.columns:
